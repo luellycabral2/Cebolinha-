2560a55cb24da64da4b97fc51ee6798bc2e72027
Luélly Cabral <luellycabral21@gmail.com>
2020-10-01 13:28:24 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cebolinha_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase; 
+
+            //<--- inicio (digite)--->//
+            Console.Write("Digite algo do seu interese");
+            frase = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($" { frase.Replace("r" , "l").Replace("R" , "L") } "); 
+            //<---fim (digite)--->//
+
+            //<---inicio(sair)--->//
+            Console.WriteLine("ConsoleKeyInfo a tecla ENTER para sair");
+            Console.ReadKey();
+            //<---fim (sair)--->//
+
+            {
+                
+            }
+        }
+    }
+}
